e342c34efbbac76645d65ca5b1766a67b0d510db
Hunter Gillane <hunter.gillane@gmail.com>
2014-08-18 10:58:12 -0600

    add signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "dashboard#show"
 
   resources :registrations, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+
+  get "signin" => "sessions#new", as: :signin
+  post "signin" => "sessions#create"
+  delete "signout" => "sessions#destroy", as: :signout
 end
